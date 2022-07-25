4fe5aefd9fbacfc139fa81e3149b8f65e27bd601
Benson <36316652+Jinb2@users.noreply.github.com>
2022-07-25 13:50:48 -0400

    slow fast

---
+++ patterns/fastAndSlowptrs/linkedListCycleTwo.py
@@ -0,0 +1,21 @@
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        slow, fast = head, head
+
+        while fast and fast.next:
+
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                break
+
+        slow2 = head
+
+        while slow2 != slow:
+
+            slow2 = slow2.next
+            slow = slow.next
+
+        return slow
