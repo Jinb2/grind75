5d16d34e52f7c5ac76362e93443798e601bf22a1
Benson <36316652+Jinb2@users.noreply.github.com>
2022-07-25 13:27:19 -0400

    added Optimized

--- patterns/twopointers/backSpaceCompare.py
+++ patterns/twopointers/backSpaceCompare.py
@@ -1,4 +1,5 @@
 from typing import *
+from unittest import skip
 
 
 class Solution:
@@ -23,7 +24,54 @@ class Solution:
 
         return stackS == stackT
 
+    def optimized(self, s, t):
+
+        i, j = len(s) - 1, len(t) - 1
+
+        skipS = 0
+        skipT = 0
+
+        while i >= 0 or j >= 0:
+
+            while i >= 0:
+
+                if s[i] == "#":
+                    skipS += 1
+                    i -= 1
+
+                elif skipS > 0:
+                    skipS -= 1
+                    i -= 1
+
+                else:
+                    break
+
+            while j >= 0:
+
+                if s[i] == "#":
+                    skipT += 1
+                    j -= 1
+
+                elif skipT > 0:
+
+                    skipT -= 1
+                    j -= 1
+
+                else:
+                    break
+
+            if i >= 0 and j >= 0 and s[i] != s[j]:
+                return False
+
+            if (i >= 0) != (j >= 0):
+                return False
+
+            i = i - 1
+            j = j - 1
+
+        return True
+
 
 print(Solution().backspaceCompare("y#fo##f", "y#f#o##f"))
 
-https://leetcode.com/problems/backspace-string-compare/discuss/585027/Python-O(N)-Time-O(1)-Space-Solution-or-Explained
+# https://leetcode.com/problems/backspace-string-compare/discuss/585027/Python-O(N)-Time-O(1)-Space-Solution-or-Explained
