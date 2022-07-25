339cf7ee9f2976ed5e0c383fa29f8ef381c614a5
Benson <36316652+Jinb2@users.noreply.github.com>
2022-07-24 23:13:11 -0400

    STACK

---
+++ patterns/twopointers/backSpaceCompare.py
@@ -0,0 +1,27 @@
+from typing import *
+
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+
+        stackS = []
+        stackT = []
+
+        for i in range(len(s)):
+
+            if s[i] == "#" and stackS:
+                stackS.pop()
+            elif s[i] != "#":
+                stackS.append(s[i])
+
+        for i in range(len(t)):
+
+            if t[i] == "#" and stackT:
+                stackT.pop()
+            elif t[i] != "#":
+                stackT.append(t[i])
+
+        return stackS == stackT
+
+
+print(Solution().backspaceCompare("y#fo##f", "y#f#o##f"))
