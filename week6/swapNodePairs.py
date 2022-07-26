2b8bc221fee02c648b8dc56fb97e68db0ec083ea
Benson <36316652+Jinb2@users.noreply.github.com>
2022-07-26 12:03:52 -0400

    pointers

---
+++ week6/swapNodePairs.py
@@ -0,0 +1,23 @@
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        if not head:
+            return head
+
+        dummy = ListNode(0, head)
+
+        prev, curr = dummy, head
+
+        while curr and curr.next:
+
+            nxtPair = curr.next.next
+            second = curr.next
+
+            second.next = curr
+            curr.next = nxtPair
+            prev.next = second
+
+            prev = curr
+            curr = nxtPair
+
+        return dummy.next
