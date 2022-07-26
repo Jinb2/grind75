1737ce59c1d496e5b83edcfb0e0d9714d6b0e13a
Benson <36316652+Jinb2@users.noreply.github.com>
2022-07-26 15:25:38 -0400

    LCS

---
+++ week6/longestConsecutiveSequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        numSet = set(nums)
+        longest = 0
+
+        for num in nums:
+
+            # check if start of sequence
+            if (num - 1) not in numSet:
+                length = 0
+
+                while num + length in numSet:
+                    length += 1
+
+                longest = max(length, longest)
+
+        return longest
+
+    # O(n)
