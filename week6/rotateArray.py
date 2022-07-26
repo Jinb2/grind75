24fef4f591f1d4e79e954556accf4896059d9c79
Benson <36316652+Jinb2@users.noreply.github.com>
2022-07-26 18:09:39 -0400

    two pointers

---
+++ week6/rotateArray.py
@@ -0,0 +1,29 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        k = k % len(nums)  # k could be > len of nums
+
+        l, r = 0, len(nums) - 1
+
+        # reverse array
+        while l < r:
+
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+
+        # reverse k first
+
+        l, r = 0, k - 1
+        while l < r:
+
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+
+        l, r = k, len(nums) - 1
+        while l < r:
+
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
