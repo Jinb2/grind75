fedc2f062b6986888ef5de79c08d8e04ae8ae010
Benson <36316652+Jinb2@users.noreply.github.com>
2022-08-01 16:53:16 -0400

    two pointer

---
+++ patterns/intervals/intervalIntersectionList.py
@@ -0,0 +1,24 @@
+class Solution:
+    def intervalIntersection(
+        self, firstList: List[List[int]], secondList: List[List[int]]
+    ) -> List[List[int]]:
+
+        # pointer for first and second list
+        i, j = 0, 0
+
+        res = []
+
+        while i < len(firstList) and j < len(secondList):
+
+            aStart, aEnd = firstList[i]
+            bStart, bEnd = secondList[j]
+
+            if aStart <= bEnd and bStart <= aEnd:
+                res.append([max(aStart, bStart), min(aEnd, bEnd)])
+
+            if aEnd <= bEnd:
+                i += 1
+            else:
+                j += 1
+
+        return res
