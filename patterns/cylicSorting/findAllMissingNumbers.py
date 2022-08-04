c6c218b736536a3209ae87d9cdfa395c968637a9
Benson <36316652+Jinb2@users.noreply.github.com>
2022-08-04 16:51:12 -0400

    cyclic sorting mark seen numbers as negative

--- patterns/cylicSorting/findAllMissingNumbers.py
+++ patterns/cylicSorting/findAllMissingNumbers.py
@@ -1,12 +1,12 @@
-    """Given an array nums of n integers where nums[i] is in the range [1, n], 
-    return an array of all the integers in the range [1, n] that do not appear in nums.
-
-    Input: nums = [4,3,2,7,8,2,3,1]
-Output: [5,6]
-
-Input: nums = [1,1]
-Output: [2]
-    """
-
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        res = []
+        # mark the seen numbers
+        for n in nums:
+
+            i = abs(n) - 1
+            nums[i] = abs(nums[i]) * -1
+
+        res = [i + 1 for i in range(len(nums)) if nums[i] > 0]
+
+        return res
