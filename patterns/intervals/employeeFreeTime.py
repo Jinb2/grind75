a01b0056d51c76fc638e80d4a3b883c2f3e12ecd
Benson <36316652+Jinb2@users.noreply.github.com>
2022-08-05 16:47:49 -0400

    binary search

--- patterns/intervals/employeeFreeTime.py
+++ patterns/intervals/employeeFreeTime.py
@@ -6,35 +6,35 @@ class Interval(object):
         self.end = end
 """
 
+
 class Solution:
     """
     @param schedule: a list schedule of employees
-    @return: Return a list of finite intervals 
+    @return: Return a list of finite intervals
     """
+
     def employee_free_time(self, schedule: List[List[int]]) -> List[Interval]:
         # Write your code here
         if not schedule:
             return []
-        
 
         # break up intervals
         intervals = []
 
         for employee in schedule:
             for interval in employee:
-                intervals.append([interval.start,interval.end])
-        
-        intervals.sort(key=lambda x : x[0])
+                intervals.append([interval.start, interval.end])
+
+        intervals.sort(key=lambda x: x[0])
 
         res = []
         lastEnd = intervals[0][1]
 
-        for i in range(1,len(intervals)):
+        for i in range(1, len(intervals)):
             start = intervals[i][0]
             if start > lastEnd:
-                res.append([Interval(lastEnd,start)])
-            
-            lastEnd = max(lastEnd,intervals[i][1])
+                res.append([Interval(lastEnd, start)])
 
+            lastEnd = max(lastEnd, intervals[i][1])
 
         return res
