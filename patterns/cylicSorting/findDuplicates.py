4edba0116d073fd67afd4b6afacfedb6cb0fe37b
Benson <36316652+Jinb2@users.noreply.github.com>
2022-08-05 17:25:51 -0400

    cycling sorting or floyd algo

--- patterns/cylicSorting/findDuplicates.py
+++ patterns/cylicSorting/findDuplicates.py
@@ -1,22 +1,19 @@
-    """
-    Find All Duplicates in an Array
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> List[int]:
 
-    Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+        i = 0
 
-You must write an algorithm that runs in O(n) time and uses only constant extra space.
+        while i < len(nums):
 
-Input: nums = [4,3,2,7,8,2,3,1]
-Output: [2,3]
+            n = nums[i]
 
-Input: nums = [1,1,2]
-Output: [1]
+            if n == i + 1:
+                i += 1
+            else:
 
-Input: nums = [1]
-Output: []
-    """
+                idx = n - 1
 
+                if nums[idx] == idx + 1:
+                    return idx + 1
 
-    class Solution:
-    def findDuplicates(self, nums: List[int]) -> List[int]:
-        
-    
+                nums[i], nums[idx] = nums[idx], nums[i]
